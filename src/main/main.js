1e18a297a99f68b10f06e72edcb7c67d05babbfe
jmwalker <walkerjulian888@gmail.com>
2025-02-24 17:11:34 -0600

    Installed Tailwind, Vite, added folders and set up preload.js, renderer.js, etc

--- src/main/main.js
+++ src/main/main.js
@@ -1,14 +1,35 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron/main')
+const path = require('node:path')
 
 const CreateWindow = () => {
     const win = new BrowserWindow({
         width: 800, 
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: true, 
+            nodeIntegration: false
+        }
     })
 
     win.loadFile('src/index.html')
+
+    win.webContents.openDevTools()
 }
 
 app.whenReady().then(() => {
+    ipcMain.handle('ping', () => 'pong')
     CreateWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            CreateWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
